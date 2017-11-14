7917bdd59c421dc1ea858b1380a004c654d3923f
Esa <Esa@Esa-PC>
2017-11-14 20:40:56 +0700

    Nilai Mahasiswa

---
+++ src/if11_10116467_latihanut3/IF11_10116467_LATIHANUT3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if11_10116467_latihanut3;
+
+import View.Volunter;
+/**
+ *
+ * @author Esa
+ */
+public class IF11_10116467_LATIHANUT3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Volunter nVol = new Volunter();
+        nVol.setVisible(true);
+    }
+
+}
